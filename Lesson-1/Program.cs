94715befc849d9124e5d9e0c1afaa0439743aa76
Igor Cebota <chebota2@gmail.com>
2022-05-04 19:19:37 +0300

    ending homework

---
+++ Lesson-1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Lesson_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Как вас зовут?");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Привет, {name}, сегодня {DateTime.Now.ToShortDateString()}");
+
+            Console.ReadLine();
+        }
+    }
+}
